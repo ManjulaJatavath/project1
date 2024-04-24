6ae270845711b7c89bb8a89a5907ede7126f5899
Manjula Jatavath <144055077+ManjulaJatavath@users.noreply.github.com>
2024-04-24 10:23:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.querySelector('.menu-icon');
     const menu = document.querySelector('.menu');
     const sections = document.querySelectorAll('section');
-    const photos = document.querySelectorAll('.photo');
-    const menuItems = document.querySelectorAll('.item'); // Select all menu items
-    let currentMenuItemIndex = 0;
+    
     
     smoothScrollLinks.forEach(link => {
         link.addEventListener('click', function (e) {
@@ -36,25 +34,28 @@ document.addEventListener('DOMContentLoaded', function () {
     menuIcon.addEventListener('click', function () {
         menu.classList.toggle('show');
     });
-    
-    // Function to handle continuous scrolling through menu items
-    function handleScroll() {
-        const threshold = window.innerHeight * 0.5; 
-        const lastMenuItem = menuItems[menuItems.length - 1];
-        
-        if (lastMenuItem.getBoundingClientRect().top <= threshold) {
-            // Show next menu item
-            currentMenuItemIndex++;
-            if (currentMenuItemIndex >= menuItems.length) {
-                currentMenuItemIndex = 0;
-                // Automatically scroll back to the first menu item
-                menuItems[currentMenuItemIndex].scrollIntoView({ behavior: 'smooth', block: 'start' });
-            } else {
-                menuItems[currentMenuItemIndex].scrollIntoView({ behavior: 'smooth', block: 'center' });
-            }
-        }
-    }
+    const contactLink = document.querySelector('a[href="#contact"]');
+    if (contactLink) {
+        contactLink.addEventListener('click', function (e) {
+            e.preventDefault();
 
-    // Add scroll event listener to trigger continuous menu item scrolling
-    window.addEventListener('scroll', handleScroll);
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const offsetTop = targetElement.getBoundingClientRect().top + window.pageYOffset;
+                window.scrollTo({
+                    top: offsetTop,
+                    behavior: 'smooth'
+                });
+                sections.forEach(section => {
+                    if (section.id === targetId) {
+                        section.classList.add('active');
+                    } else {
+                        section.classList.remove('active');
+                    }
+                });
+            }
+        });
+    }
 });
